5f88ff3f7c403c97ce53477313628a9fb89e8b02
johntrejosl <53836288+johntrejosl@users.noreply.github.com>
2019-10-31 16:15:23 -0500

    Add files via upload

---
+++ ingresar.php
@@ -0,0 +1,48 @@
+
+<html>
+<body>
+Hola <?php 
+
+/*$reset=$_POST['reset'];
+
+
+
+foreach ($fichas as $ficha) {
+	
+fwrite($fp, '<#'.$rompecabezas->ficha['id'].'{'. PHP_EOL);
+fwrite($fp, ' transform: rotate('.$rompecabezas->ficha->posicionAsociada['grados'].');'. PHP_EOL);
+fwrite($fp, '}'. PHP_EOL);
+fclose($fp);
+
+}
+ */
+
+
+
+
+echo $_POST["nombre"]; ?><br>
+edad: <?php echo $_POST["edad"];
+
+
+$fp = fopen("registro.xml","w+");
+fwrite($fp, '<?xml version="1.0" encoding="UTF-8" ?> '. PHP_EOL);
+
+fclose($fp);
+?> <br>
+<?php
+echo "escrito";
+ ?>
+
+
+
+
+</body>
+</html>
+
+	
+
+
+
+
+    
+
